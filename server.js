87681aa670341c2c88cca4d93e12299ff3bdacb2
J Powers <juliepowers@cmail.carleton.ca>
2020-10-02 16:15:22 -0400

    Fix typo found while testing with Postman

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get('/users/:id', function(req, res) {
 })
 
 // login endpoint
-app.post('login', function(req, res) {
+app.post('/login', function(req, res) {
     const username = req.body.username;
     const password = req.body.password;
 
